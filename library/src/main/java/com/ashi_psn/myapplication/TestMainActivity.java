516dc2edf4d0074039aaca6afc936a227cf9c0c5
ashi-psn <naoya.ashizawa@welby.jp>
2016-12-09 12:48:53 +0900

    change

---
+++ library/src/main/java/com/ashi_psn/myapplication/TestMainActivity.java
@@ -0,0 +1,13 @@
+package com.ashi_psn.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TestMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_main);
+    }
+}
